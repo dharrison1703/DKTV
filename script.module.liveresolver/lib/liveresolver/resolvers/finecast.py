11ffcbd52e240db4c302f526f53c1f5cb548692d
dharrison1703 <d.harrison1703@gmail.com>
2016-03-21 20:34:25 +0000

    quickfix

--- script.module.liveresolver/lib/liveresolver/resolvers/finecast.py
+++ script.module.liveresolver/lib/liveresolver/resolvers/finecast.py
@@ -7,27 +7,22 @@ from liveresolver.modules import jsunpack,decryptionUtils
 
 def resolve(url):
     try:
-        try:
-            referer = urlparse.parse_qs(urlparse.urlparse(url).query)['referer'][0]
-        except:
-            referer=url
+		try:
+			referer = urlparse.parse_qs(urlparse.urlparse(url).query)['referer'][0]
+		except:
+			referer=url
 
+		id = urlparse.parse_qs(urlparse.urlparse(url).query)['u'][0]
+		url = 'http://www.finecast.tv/embed4.php?u=%s&width=640&height=450&vh=450&vw=640'%id
+		headers=[("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:37.0) Gecko/20100101 Firefox/37.0"),
+		("Referer", referer)]
 
-        id = urlparse.parse_qs(urlparse.urlparse(url).query)['u'][0]
-        url = 'http://www.finecast.tv/embed4.php?u=%s&vw=640&vh=450'%id
-        headers=[("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:37.0) Gecko/20100101 Firefox/37.0"),
-                 ("Referer", referer)]
+		cj = cookielib.LWPCookieJar()
+		result = unCaptcha.performCaptcha(url, cj, headers = headers)
+		file = re.findall('[\'\"](.+?.stream)[\'\"]',result)[0]
+		auth = re.findall('[\'\"](\?wmsAuthSign.+?)[\'\"]',result)[0]
+		url = 'http://play.finecast.tv/live/'+file+'/playlist.m3u8'+auth
+		return url
 
-
-        cj = cookielib.LWPCookieJar()
-        result = unCaptcha.performCaptcha(url, cj, headers = headers)
-        file = re.findall('[\'\"](.+?.stream)[\'\"]',result)[0]
-        auth = re.findall('[\'\"](\?wmsAuthSign.+?)[\'\"]',result)[0]
-        rtmp = 'rtmp://play.finecast.tv:1935/live%s'%auth
-        url = rtmp +  ' playpath=' + file + ' swfUrl=http://www.finecast.tv/player6/jwplayer.flash.swf flashver=WIN\2020,0,0,267 live=1 timeout=14 pageUrl=' + url
-        return url
-
-        
     except:
         return
-
